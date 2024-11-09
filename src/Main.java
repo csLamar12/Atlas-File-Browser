82674b5563ab1d5bd01e1d6f8f5171986fbf588d
csLamar12 <90802465+csLamar12@users.noreply.github.com>
2024-11-09 00:02:53 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import Controller.FileBrowser;
+import Model.FileNode;
+
+import java.io.File;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        FileBrowser fileBrowser = new FileBrowser();
+        File root = new File("/");
+        File[] files = root.listFiles();
+        for (File file : files) {
+//            System.out.println(file.getName());
+            File[] files2 = file.listFiles();
+            if (files2 != null) {
+                for (File file2 : files2) {
+                    System.out.println(file.getName() + "/" + file2.getName());
+                }
+            }
+        }
+//        FileNode rootNode = new FileNode(root);
+//        fileBrowser.listFilesOf(rootNode);
     }
 }
